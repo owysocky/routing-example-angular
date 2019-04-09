d76d349080f0f1b561d99757858cd839f7e56170
Guest User <Guest@epicodus-9.local>
2019-04-09 15:46:22 -0700

    tie fire data base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -9,6 +11,12 @@ import { AboutComponent } from './about/about.component';
 import { MarketpalceComponent } from './marketpalce/marketpalce.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
