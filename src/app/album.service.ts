9cddf84fcedf06bd8119adc18b852b22483b6e7c
Guest User <Guest@epicodus-9.local>
2019-04-09 14:40:10 -0700

    add injection file

---
+++ src/app/album.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Album } from './models/album.model';
+import { ALBUMS } from './mock-albums';
+
+@Injectable()
+export class AlbumService {
+
+  constructor() { }
+
+}
